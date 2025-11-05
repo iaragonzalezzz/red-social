feb249bef9000865490685585ed8f9bd989acd37
Iara González <iaragonzalez521@gmail.com>
2025-11-05 11:03:26 -0300

    arreglos hechos

--- src/app/perfil/mi-perfil.component.ts
+++ src/app/perfil/mi-perfil.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mi-perfil',
-  template: '<h2>Mi Perfil</h2><p>Datos del usuario (Sprint #2)</p>',
+  template: '<h2>Mi Perfil</h2><p>Datos del usuario</p>',
   styleUrls: ['./mi-perfil.component.css']
 })
 export class MiPerfilComponent {}
